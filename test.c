2d275c0668d494f617beb155d46b9b437acc905d
UmiUe <Sungrazer@foxmail.com>
2019-11-08 17:36:33 +0800

    test

--- test.c
+++ test.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#define N 7
 
 int main()
 {
-    printf("Hello World");
+    int a[N] = {2, 4, 7, 3, 9, 1, 6};
+
+    for (int i = 1; i < N; i++)
+    {
+        int j = i - 1, key = a[i];
+        while (j >= 0 && a[j] > key)
+        {
+            a[j+1] = a[j];
+            j--;
+        }
+
+        a[j+i]=key;
+    }
+
+    for(int i = 0; i < N; i++)
+    {    
+        printf("%d", a[i]);
+    }
+    printf("\n");
+
     return 0;
 }
