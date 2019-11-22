1957b6e982746acb6f5e3f500fbe4c6333183e07
UmiUe <sungrazer@foxmail.com>
2019-11-22 13:23:07 +0800

    a

---
+++ bst.c
@@ -0,0 +1,34 @@
+#include "bst.h"
+#include <stdlib.h>
+
+void newNode(struct node *root, int n)
+{
+    struct node *p = malloc(sizeof(struct node));
+    if (p == NULL)
+    {
+        printf("Error\n");
+        exit(-1);
+    }
+    p->key = n;
+    p->left = p->right = NULL;
+}
+
+void insert(struct node *root, int n)
+{
+    if(root == NULL)
+    {
+        root = newNode(n);
+    }
+    if(root->key > n)
+    {
+        insert(root->left, n);
+    }
+    else if (root->key < n)
+    {
+        insert(root->right, n);
+    }
+    else
+    {
+        return ;
+    }
+}
